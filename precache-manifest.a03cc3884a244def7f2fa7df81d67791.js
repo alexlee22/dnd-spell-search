eea2c30cba7fa68e6fc22cb23555603fa39829b0
Alexander Lee <10814755+alexlee22@users.noreply.github.com>
2020-01-10 17:02:03 +1100

    Updates

---
+++ precache-manifest.a03cc3884a244def7f2fa7df81d67791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e53c48c2ebe0108330c2311d673f931",
+    "url": "/dnd-spell-search/index.html"
+  },
+  {
+    "revision": "95cfa488f7c785c88e2c",
+    "url": "/dnd-spell-search/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e8e78708bede87ece328",
+    "url": "/dnd-spell-search/static/js/2.86c75166.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dnd-spell-search/static/js/2.86c75166.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95cfa488f7c785c88e2c",
+    "url": "/dnd-spell-search/static/js/main.d7037ea2.chunk.js"
+  },
+  {
+    "revision": "3a7863d1093884affa61",
+    "url": "/dnd-spell-search/static/js/runtime-main.0931aee7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dnd-spell-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
